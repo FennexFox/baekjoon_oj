703c1d74cf7fb5b98c58e1a3226b4e249dad3887
AnotherFireFox <technocracy90@gmail.com>
2025-12-16 17:33:51 +0900

    test 11003: pass

--- 11003.py
+++ 11003.py
@@ -1,17 +1,19 @@
 # 나머지 합 (https://www.acmicpc.net/problem/11003)
 # Platinum 5
 # Data Structure, Prioirity Queue, Deque, Sliding Window Maximum using a Deque
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 num_len, window_len = map(int, input().split())
-nums = map(int, input().split())
+nums = list(map(int, input().split()))
 window = deque()
 
-for i, num in enumerate(nums):
-    while window and window[-1][0] > num:
+for i in range(len(nums)):
+    while window and nums[window[-1]] > nums[i]:
         window.pop()
-    window.append([num, i])
-    if i - window[0][1] >= window_len:
+    window.append(i)
+    if i - window[0] >= window_len:
         window.popleft()
     
-    print(window[0][0], end=" ")
+    print(nums[window[0]], end=" ")
