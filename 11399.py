b9420a1162bdfa8c2916a7147faf5fba148bb433
AnotherFireFox <technocracy90@gmail.com>
2025-12-18 17:12:08 +0900

    test 11399: using custom sort

--- 11399.py
+++ 11399.py
@@ -2,11 +2,18 @@
 # tier: Silver 4
 # tags: Greedy, Sort
 
-length = int(input())
-array = list(map(int, input().split()))
+len = int(input())
+array = list(map(int, input().split())) + [float("inf")]
 time = 0
 
-for i, t in enumerate(sorted(array)):
-    time += (length - i) * t
+for j in range(len):
+    for i in range(j):
+        if array[i] < array[i+1]:
+            array[i], array[i+1] = array[i+1], array[i]
+
+array = array[:-1]
+
+for i, t in enumerate(array):
+    time += (i+1) * t
 
 print(time)
