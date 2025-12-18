bb03c302384f5702e8463722a55d2a6396a250f1
AnotherFireFox <technocracy90@gmail.com>
2025-12-18 13:53:32 +0900

    test 1629: pass

--- 1629.py
+++ 1629.py
@@ -4,25 +4,13 @@
 
 num1, num2, num3 = map(int, input().split())
 num1 %= num3
-num4, record = num1, []
+num4 = 1
 
-for i in range(num2):
-    num4 %= num3
-    if record and num4 in record:
-        break
-    
-    record.append(num4)
-    num4 *= num1
+while num2:
+    if num2 % 2 != 0:
+        num4 = (num4 * num1) % num3
+    num1 *= num1
+    num1 %= num3
+    num2 //= 2
 
-loop_start = 0
-
-try:
-    loop_start = record.index(num4)
-except:
-    pass
-
-record = record[loop_start:]
-num2 -= loop_start
-cycle_len = len(record)
-
-print(record[(num2 - 1)%cycle_len])
+print(num4)
