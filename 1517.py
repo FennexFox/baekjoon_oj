298fe89762bf20dd28240d531cc181f8c837193a
AnotherFireFox <technocracy90@gmail.com>
2025-12-19 11:24:58 +0900

    test 1517: pass (helped)

--- 1517.py
+++ 1517.py
@@ -2,14 +2,36 @@
 # tier: Platinum 5
 # tags: Data Structure, Sort, Segment Tree, Divide and Conquer
 
-length = int(input())
+_ = int(input())
 array = list(map(int, input().split()))
 count = 0
 
-for i in range(length-1, -1, -1):
-    for j in range(i):
-        if array[j] > array[j+1]:
-            count += 1
-            array[j], array[j+1] = array[j+1], array[j]
-            
+def split_and_merge(array):
+    midpoint = len(array)//2
+    
+    if midpoint == 0:
+        return array
+    else:
+        left = split_and_merge(array[:midpoint])
+        right = split_and_merge(array[midpoint:])
+        return sort(left, right)
+
+def sort(left, right):
+    array = []
+    global count
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            array.append(left[i])
+            i += 1
+        else:
+            array.append(right[j])
+            j += 1
+            count += len(left) - i
+    
+    array += right[j:] if i == len(left) else left[i:]
+    return array
+
+array = split_and_merge(array)
 print(count)
