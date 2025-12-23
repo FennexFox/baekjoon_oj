fcbb57cfdf465ef2c757ba48c7779800bbc2ce75
AnotherFireFox <technocracy90@gmail.com>
2025-12-23 17:33:28 +0900

    test 11725: without class

--- 11725.py
+++ 11725.py
@@ -2,44 +2,23 @@
 # tier: Silver 2
 # tags: Graph, Tree, DFS, BFS
 
-import sys
-input = sys.stdin.readline
-print = sys.stdout.write
-sys.setrecursionlimit(200000)
-
-class Node:
-    def __init__(self, i):
-        self.name = i
-        self.parent = None
-        self.unsorted_links = set()
-    
-    def __call__(self, other):
-        self.unsorted_links.add(other)
-        other.unsorted_links.add(self)
-    
-    def __str__(self):
-        return (str(self.parent.name) + "\n") if self.parent else ""
-
-    def dfs(self):
-        while self.unsorted_links:
-            link = self.unsorted_links.pop()
-            if self in link.unsorted_links:
-                link.unsorted_links.remove(self)
-            link.parent = self
-            link.dfs()
-            
-        
 length = int(input())
-graph = [Node(i+1) for i in range(length)]
+visited = [False] * (length + 1)
+parent = [i for i in range(length + 1)]
+linked_nodes = {i+1: [] for i in range(length)}
 
-for _ in range(length-1):
-    node1, node2 = [graph[i-1] for i in map(int, input().split())]
-    node1(node2)
+for i in range(length-1):
+    node1, node2 = map(int, input().split())
+    linked_nodes[node1].append(node2)
+    linked_nodes[node2].append(node1)
 
-graph[0].dfs()
+def dfs(node):
+    visited[node] = True
+    for other_node in linked_nodes[node]:
+        if not visited[other_node]:
+            dfs(other_node)
+            parent[other_node] = node
 
-out = []
-for i in range(length):
-    out.append(graph[i])
+dfs(1)
 
-print("".join(map(str, out)))
+print('\n'.join(map(str, parent[2:])))
