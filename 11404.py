7bdde933def85456ef1390dd31c38a8a4b90ad95
AnotherFireFox <technocracy90@gmail.com>
2025-12-26 16:03:24 +0900

    test 11404: optimize (helped)

--- 11404.py
+++ 11404.py
@@ -6,23 +6,38 @@ import sys
 input = sys.stdin.readline
 print = sys.stdout.write
 
-INF = 10**9 + 1
+INF = 10**15
 city_num = int(input()) + 1
+bus_num = int(input())
 
-fares = [[INF for _ in range(city_num)] for _ in range(city_num)]
+fares = [[INF] * (city_num) for _ in range(city_num)]
+for i in range(1, city_num):
+    fares[i][i] = 0
 
-for _ in range(int(input())):
+for _ in range(bus_num):
     start, end, fare = map(int, input().split())
-    fares[start][end] = min(fare, fares[start][end])
+    if fares[start][end] > fare:
+        fares[start][end] = fare
 
-for k in range(1, city_num):
+for m in range(1, city_num):
+    row_m = fares[m]
     for i in range(1, city_num):
+        row_i = fares[i]
+        fare_im = row_i[m]
+        if fare_im == INF:
+            continue
         for j in range(1, city_num):
-            if i != j and fares[i][j] > fares[i][k] + fares[k][j]:
-                fares[i][j] = fares[i][k] + fares[k][j]
+            fare_mj = row_m[j]
+            if fare_mj == INF:
+                continue
+            fare_ij = row_i[j]
+            fare_imj = fare_im + fare_mj
+            if fare_ij > fare_imj:
+                row_i[j] = fare_imj
 
-out = []
-for row in range(1, city_num):
-    out.append(" ".join(str(x).replace("1000000001", "0") for x in fares[row][1:]))
+out_lines = []
+for i in range(1, city_num):
+    row_i = fares[i]
+    out_lines.append(" ".join("0" if row_i[j] == INF else str(row_i[j]) for j in range(1, city_num)))
 
-print("\n".join(out))
+print("\n".join(out_lines))
