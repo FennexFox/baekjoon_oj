2ff4c2025b6246f20e2cc02d9dbe2942a46d1aa3
AnotherFireFox <technocracy90@gmail.com>
2026-01-06 12:24:56 +0900

    test 1238: improved

--- 1238.py
+++ 1238.py
@@ -9,15 +9,17 @@ print = sys.stdout.write
 INF = 10 ** 6
 n, m, x = map(int, input().split())
 paths = [[] for _ in range(n+1)]
+reverse_paths = [[] for _ in range(n+1)]
 
 for _ in range(m):
     start, end, time = map(int, input().split())
     paths[start].append([end, time])
+    reverse_paths[end].append([start, time])
 
-def dijkstra(start, is_returning = False):
-    p_queue = [(0, start)]
+def dijkstra(graph):
+    p_queue = [(0, x)]
     visited, times = [False] * (n+1), [INF] * (n+1)
-    times[start] = 0
+    times[x] = 0
 
     while p_queue:
         this = heapq.heappop(p_queue)
@@ -27,24 +29,15 @@ def dijkstra(start, is_returning = False):
             continue
         visited[this_node] = True
         
-        for this_edge in paths[this_node]:
+        for this_edge in graph[this_node]:
             next_node = this_edge[0]
             next_time = this_edge[1]
             if times[next_node] > times[this_node] + next_time:
                 times[next_node] = times[this_node] + next_time
                 heapq.heappush(p_queue, (times[next_node], next_node))
     
-    if is_returning:
-        return times[x]
-    else:
-        return times
+    return times
 
-times = dijkstra(x)
+times = [x+y for x, y in zip(dijkstra(paths)[1:], dijkstra(reverse_paths)[1:])]
 
-for start in range(1, n+1):
-    if start == x:
-        continue
-    else:
-        times[start] += dijkstra(start, True)
-
-print(str(max(times[1:])))
+print(str(max(times)))
